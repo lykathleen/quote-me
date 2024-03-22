f0dd72d082c659ddc704be83f2c2e968346038d5
lykathleen <lykathleen@users.noreply.github.com>
2024-03-22 17:57:12 +0000

    Extracted quote-text out

--- src/components/QuoteBox/QuoteBox.jsx
+++ src/components/QuoteBox/QuoteBox.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Button from '../Button/Button'
 import styles from './QuoteBox.module.scss';
+import QuoteText from '../QuoteText/QuoteText';
 
 const QuoteBox = () => {
   return (
       <div className={styles.quoteBox}>
-        BOX HERE
-        <div className={styles.text}></div>
+        <div className={styles.text}><QuoteText/></div>
         <div className={styles.author}></div>
         <div className={styles.buttons}>
           <Button className={styles.socialMediaButton} text='facebook'/>
---
+++ src/components/QuoteText/QuoteText.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const QuoteText = () => {
+  return (
+    <>
+      <div className="quote-text">"QUOTE HERE"</div>
+      <div className="quote-author">"AUTHOR"</div>
+    </>
+  );
+};
+
+export default QuoteText;
