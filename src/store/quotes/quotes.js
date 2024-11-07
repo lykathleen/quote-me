ff55945fb948f51953eff8ab804b82ea6b12c667
lykathleen <lykathleen@users.noreply.github.com>
2024-11-07 17:35:10 +0000

    Included more quotes

--- src/store/quotes/quotes.js
+++ src/store/quotes/quotes.js
@@ -14,6 +14,46 @@ const quotes = [
   {
     text: 'Definiteness of purpose is the starting point of all achievement.',
     author: '- W. Clement Stone'
+  },
+  {
+    text: 'The only way to do great work is to love what you do.',
+    author: 'Steve Jobs'
+  },
+  {
+    text: "Believe you can and you're halfway there.",
+    author: 'Theodore Roosevelt'
+  },
+  {
+    text: 'You are never too old to set another goal or to dream a new dream.',
+    author: 'C.S. Lewis'
+  },
+  {
+    text: "It always seems impossible until it's done.",
+    author: 'Nelson Mandela'
+  },
+  {
+    text: 'What lies behind us and what lies before us are tiny matters compared to what lies within us.',
+    author: 'Ralph Waldo Emerson'
+  },
+  {
+    text: 'Success is not final, failure is not fatal: It is the courage to continue that counts.',
+    author: 'Winston Churchill'
+  },
+  {
+    text: "You miss 100% of the shots you don't take.",
+    author: 'Wayne Gretzky'
+  },
+  {
+    text: 'The best time to plant a tree was 20 years ago. The second best time is now.',
+    author: 'Chinese Proverb'
+  },
+  {
+    text: 'The only limit to our realization of tomorrow is our doubts of today.',
+    author: 'Franklin D. Roosevelt'
+  },
+  {
+    text: 'Happiness is not something ready made. It comes from your own actions.',
+    author: 'Dalai Lama'
   }
 ];
 
